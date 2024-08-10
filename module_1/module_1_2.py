48c1e927d3a3663a33052eba7cf9fa85bf4dc03a
l3t-it-be <ka7e.online@gmail.com>
2024-08-10 12:47:03 +0300

    homework 2 added

---
+++ module_1/module_1_2.py
@@ -0,0 +1,9 @@
+homework_amount = 12
+hours_spent = 1.5
+course_name = "Python"
+time_for_1_homework = hours_spent / homework_amount
+
+print(
+    f'Курс: {course_name}, всего задач: {homework_amount}, затрачено часов: {hours_spent}, '
+    f'среднее время выполнения {time_for_1_homework} часа.'
+)
