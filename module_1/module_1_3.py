16170663191f6c032f49884dbe4a74cf5624a83f
l3t-it-be <ka7e.online@gmail.com>
2024-08-11 15:14:24 +0300

    3rd homework added

---
+++ module_1/module_1_3.py
@@ -0,0 +1,12 @@
+name = 'Kate'
+print(f'Name: {name}')
+
+age = 18
+print(f'Age: {age}')
+
+new_age = 20
+age = new_age
+print(f'New Age: {age}')
+
+is_student = True
+print(f'Is Student: {is_student}')
