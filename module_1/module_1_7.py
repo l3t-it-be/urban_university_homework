55bc5d09784019445d66fce8951e3348a9a50bbc
l3t-it-be <ka7e.online@gmail.com>
2024-08-14 19:14:24 +0300

    7th homework added

---
+++ module_1/module_1_7.py
@@ -0,0 +1,11 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+students_list = sorted(list(students))
+
+avg_grades = {}
+
+for i in range(len(students_list)):
+    avg_grades[students_list[i]] = sum(grades[i]) / len(grades[i])
+
+print(avg_grades)
