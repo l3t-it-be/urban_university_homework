823c720c11a52967f12892a4d48b5955891d9375
l3t-it-be <ka7e.online@gmail.com>
2024-08-21 00:14:28 +0300

    13th homework fixed

--- module_2/module_2_hard.py
+++ module_2/module_2_hard.py
@@ -1,13 +1,18 @@
-def get_password(num):
+def generate_password(num):
     result = ''
+
     for i in range(1, num):
-        for j in range(i, num + 1):
-            if (i + j) % num == 0:
-                result += str(i) + str(j)
+        for j in range(i + 1, num + 1):
+            pair_sum = i + j
+            if num % pair_sum == 0:
+                result += f'{i}{j}'
 
     return result
 
 
 n = int(input('Введите число от 3 до 20: '))
-password = get_password(n)
-print(f'Пароль для числа {n}: {password}')
+if 3 <= n <= 20:
+    password = generate_password(n)
+    print(f'Нужный пароль для {n}: {password}')
+else:
+    print('Число должно быть в диапазоне от 3 до 20')
