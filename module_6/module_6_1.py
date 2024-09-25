f52c01529b9da6a026cc5c2634864871deab8677
KateBelova <ka7e.online@gmail.com>
2024-09-25 20:44:08 +0300

    27th homework fixed

--- module_6/module_6_1.py
+++ module_6/module_6_1.py
@@ -1,60 +1,50 @@
 class Animal:
+    alive = True
+    fed = False
+
     def __init__(self, name):
-        self.alive = True
-        self.fed = False
         self.name = name
+
+    def eat(self, food):
+        if food.edible:
+            self.fed = True
+            return f'{self.name} съела {food.name}'
+        else:
+            self.alive = False
+            return f'{self.name} не стала есть {food.name}'
 
 
 class Mammal(Animal):
-    def __init__(self, name):
-        super().__init__(name)
-
-    def eat(self, food):
-        if food.edible:
-            self.fed = True
-            return f'{self.name} съела {food.name}'
-        else:
-            self.alive = False
-            return f'{self.name} не стала есть {food.name}'
+    pass
 
 
 class Predator(Animal):
-    def __init__(self, name):
-        super().__init__(name)
-
-    def eat(self, food):
-        if food.edible:
-            self.fed = True
-            return f'{self.name} съела {food.name}'
-        else:
-            self.alive = False
-            return f'{self.name} не стала есть {food.name}'
+    pass
 
 
 class Plant:
+    edible = False
+
     def __init__(self, name):
-        self.edible = False
         self.name = name
 
 
 class Flower(Plant):
-    def __init__(self, name):
-        super().__init__(name)
-        self.edible = False
+    pass
 
 
 class Fruit(Plant):
-    def __init__(self, name):
-        super().__init__(name)
-        self.edible = True
+    edible = True
 
 
+# Создание экземпляров
 apatosaurus = Mammal('Long neck')
 velociraptor = Predator('Blue')
 
 daisy = Flower('Mayweed')
 orange = Fruit('Clockwork Orange')
 
+# Проверка значений
 print(apatosaurus.name)
 print(velociraptor.name)
 print(daisy.name)
@@ -63,8 +53,10 @@ print(orange.name)
 print(apatosaurus.alive)
 print(velociraptor.fed)
 
+# Выполнение методов
 print(apatosaurus.eat(orange))
 print(velociraptor.eat(daisy))
 
+# Проверка значений после еды
 print(apatosaurus.alive)
 print(velociraptor.fed)
