d982bc5005a7d4691cd778cadf3050188b504d7d
KateBelova <ka7e.online@gmail.com>
2024-10-12 18:32:54 +0300

    36th homework added

---
+++ module_8/module_8_1.py
@@ -0,0 +1,12 @@
+def add_everything_up(a, b):
+    try:
+        res = a + b
+    except TypeError:
+        return str(a) + str(b)
+
+    return round(res, 3)
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
