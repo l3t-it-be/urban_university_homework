1710b192195b5d91192730857785ea5c4451b065
KateBelova <ka7e.online@gmail.com>
2024-10-27 13:57:59 +0300

    46th homework added

---
+++ module_10/module_10_1.py
@@ -0,0 +1,42 @@
+import threading
+import time
+
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i in range(1, word_count + 1):
+            file.write(f'Какое-то слово № {i}\n')
+            time.sleep(0.1)
+
+    print(f'Завершилась запись в файл {file_name}')
+
+
+start_time_functions = time.time()
+
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+
+end_time_functions = time.time()
+print(f'Работа функций: {end_time_functions - start_time_functions: .2f} сек')
+
+start_time_threads = time.time()
+
+threads = []
+
+for count, filename in [
+    (10, 'example5.txt'),
+    (30, 'example6.txt'),
+    (200, 'example7.txt'),
+    (100, 'example8.txt'),
+]:
+    thread = threading.Thread(target=write_words, args=(count, filename))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+end_time_threads = time.time()
+print(f'Работа потоков: {end_time_threads - start_time_threads: .2f} сек')
